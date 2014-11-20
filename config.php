35c1280bbfebb6adc4d6f1665c16b08a714ecee0
abhil <abhilash@alraislabs.com>
2014-11-20 11:46:47 +0530

    initial

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Database config variables
+ */
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD", "12345");
+define("DB_DATABASE", "gcm");
+
+/*
+ * Google API Key
+ */
+define("GOOGLE_API_KEY", "AIzaSyCNdC4tbY8EEyOZU3m5fgtPPLZTpSV_fBY"); // Place your Google API Key
+?>
